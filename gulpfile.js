66016fb41b23884a7f9276297798366c212fd9b0
Stockbridge <chris.d.stockbridge@gmail.com>
2015-08-02 17:46:53 -0700

    fixing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var nib = require('nib');
 var del = require('del');
 
 // compile HBS templates to src/js
-gulp.task('templates', function(){
+gulp.task('templates', function () {
   gulp.src('src/templates/**/*.hbs')
     .pipe(handlebars())
     .pipe(wrap('Handlebars.template(<%= contents %>)'))
@@ -23,21 +23,21 @@ gulp.task('templates', function(){
 });
 
 // clean dist folder
-gulp.task('clean', function(cb) {
-  del(['dist/*'], cb);
+gulp.task('clean', function () {
+  del(['dist/*']);
 });
 
 // compile minified scripts
-gulp.task('scripts-build', ['templates'], function() {
-  return gulp.src('src/js/**/*.js')
+gulp.task('scripts-build', ['templates'], function () {
+  return gulp.src(['src/js/*.js', 'src/js/**/*.js'])
     .pipe(uglify())
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/js'));
 });
 
 // compile scripts
-gulp.task('scripts', ['templates'], function() {
-  return gulp.src('src/js/**/*.js')
+gulp.task('scripts', ['templates'], function () {
+  return gulp.src(['src/js/*.js', 'src/js/**/*.js'])
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/js'));
 });
@@ -55,14 +55,14 @@ gulp.task('stylus', function () {
     .pipe(stylus({ use: nib(), compress: false, linenos: true }))
     .pipe(gulp.dest('dist/css'));
 });
- 
+
 // watch on compile
-gulp.task('watch', function() {
-  gulp.watch('src/js/**/*/js', ['scripts']);
-  gulp.watch('src/styl/**/*.hbs', ['stylus']);
+gulp.task('watch', function () {
+  gulp.watch(['src/js/*.js', 'src/js/**/*.js'], ['scripts']);
+  gulp.watch('src/styl/**/*.styl', ['stylus']);
 });
 
- 
+
 // default task
 gulp.task('build', ['clean', 'scripts-build', 'stylus-build']);
 gulp.task('default', ['clean', 'scripts', 'stylus', 'watch']);
