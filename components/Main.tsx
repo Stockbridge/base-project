03a904fe321467cd45e9691f26fba60c951e1a07
Chris Stockbridge <stockbrc@imdb.com>
2023-09-15 21:23:11 -0700

    ts and styled components ready

---
+++ components/Main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Main = () => {
+  return (
+    <main>
+      <h1>This is a starter project</h1>
+    </main>
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+import { Main } from "../components/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Stockbridge - Base Project</title>
+      </Head>
+      <Main />
+    </>
+  );
+}
